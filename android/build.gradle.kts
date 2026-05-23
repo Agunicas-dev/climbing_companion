944e0a09beef4fb6f25a8acbb9de76b1d358de6e
Agunicas-dev <agunicas@gmail.com>
2026-05-23 19:27:47 +0200

    added a lot of things. Final touches in order to deliver a first version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,7 +16,12 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileSdkVersion(36)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
